dda440698ed20070a72589be2be6fa89494ea320
Everson Faria <faria.everson2017@hotmail.com>
2024-04-19 15:54:04 -0300

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -12,25 +12,8 @@ let num = ref(0)
       <router-link class="navbar-brand" to="/">
         <img src="https://cdn-icons-png.flaticon.com/512/188/188987.png" alt="logo" width="30" heigth="24" class="d-inline-block align-text-top">
         PokeVite
-      </router-Link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link" aria-current="page" to="/Home">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/Features">Features</router-link>
-          </li>
-          <!-- <li class="nav-item">
-            <router-link class="nav-link" href="#">Pricing</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link disabled" aria-disabled="true">Disabled</router-link>
-          </li> -->
-        </ul>
+      </router-Link> 
+        <div class="collapse navbar-collapse" id="navbarNav">
       </div>
     </div>
   </nav>
