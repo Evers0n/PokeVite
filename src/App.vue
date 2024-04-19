78202ce26cbea403bc16fa6f7c2b76c78b76ff12
Everson Faria <faria.everson2017@hotmail.com>
2024-04-18 21:51:28 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import {ref} from 'vue'
+
+let num = ref(0)
+
+</script>
+
+<template>
+  <!--Navbar importada do bootstrap-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">
+        <img src="https://cdn-icons-png.flaticon.com/512/188/188987.png" alt="logo" width="30" heigth="24" class="d-inline-block align-text-top">
+        PokeVite
+      </router-Link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/Home">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/Features">Features</router-link>
+          </li>
+          <!-- <li class="nav-item">
+            <router-link class="nav-link" href="#">Pricing</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link disabled" aria-disabled="true">Disabled</router-link>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Content, para usar o Router -->
+  <div class="main">
+    <router-view>
+
+    </router-view>
+  </div>
+
+
+  <!--Footer do projeto-->
+ <footer class="bg-primary text-light">
+  <div>
+    Criado em {{ new Date().getFullYear() }}
+  </div>
+  </footer>
+</template>
+
+<style>
+body {
+  height: 100vh;
+  background: rgb(25, 136, 180);
+  background: linear-gradient(0deg, rgba(106, 196, 233, 0.9360119047619048) 0%, rbga(0,41,150,1) 100%);
+}
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 56px;
+}
+
+
+</style>
---
+++ src/components/CardPokemonSelected.vue
@@ -0,0 +1,43 @@
+<script setup>
+    const pokemon = defineProps(["name", "xp", "height", "img" ,"loading", "id",])
+</script>
+
+<template>
+    <div class="card CardPokemonSelected"
+    
+    :class="loading ? '' : 'animate__animated animate__flipInY'">
+        <img v-if="pokemon.name" :src="pokemon.img"  height="250" class="card-img-top pt-2" :alt="pokemon.name">
+        <img v-else src="../assets/egg_pokemon.svg"  height="250" class="card-img-top pt-2" alt="???">
+
+        <div class="card-body">
+            <h5 class="card-title text-center">{{pokemon.name || '???'}}</h5>
+            <hr>
+            <div class="row">
+                <section class="col">
+                    <strong>XP: </strong>
+                    <span>{{pokemon.xp}}</span>
+                </section>
+
+                <section class="col">
+                    <strong>Altura: </strong>
+                    <span>{{ pokemon.height}}</span>
+                </section>
+
+                <section class="col">
+                    <strong>Id: </strong>
+                    <span>{{ pokemon.id}}</span>
+                </section>
+            </div>
+           
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+    .CardPokemonSelected{
+        height: 75vh;
+        background: rgb(72, 63, 251);
+        background: radial-gradient(circle, rgba(216, 150, 50, 0.8) 0%, rgba(254,128,24,0.5) 100%);    
+    }
+</style>
